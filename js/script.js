95ecd7f64845f783e1372eed3cae840f2bd8352c
Michael Mroczka <michael.mroczka@ge.com>
2017-07-14 11:27:58 -0400

    Javascript timer on page works

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
 document.getElementById("DataPost").addEventListener("click", function(){
-
     document.getElementById("DataFeed").innerHTML = "<p>Voltage: 0.3</p><p>Temperature: 25.39</p>";
 });
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        display.textContent = minutes + ":" + seconds;
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var fiveMinutes = 60 * 5,
+        display = document.querySelector('#DataFeed');
+    startTimer(fiveMinutes, display);
+};
